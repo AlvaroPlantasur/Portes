3fef208141f6bfca11fee9c413685496bfba029a
AlvaroPM <alvaropavonmartinez7@gmail.com>
2025-05-08 14:04:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,94 +33,95 @@ def main():
     
     # 3. Consulta SQL con el rango dinámico
     query = f"""
-    SELECT
-        ai.id AS "ID FACTURA",
-        ai.date_invoice AS "FECHA FACTURA",
-        ai.internal_number AS "CÓDIGO FACTURA",
-        ai.name AS "DESCRIPCION",
-        rc.name AS "COMPAÑÍA",
-        ssp.name AS "SEDE",
-        'S-' || rp.id AS "ID BBSeeds",
-        rp.nombre_comercial AS "CLIENTE",
-        rpa.city AS "CIUDAD",
-        CASE
-            WHEN rpa.prov_id IS NOT NULL THEN
-                (SELECT UPPER(name) FROM res_country_provincia WHERE id = rpa.prov_id)
-            ELSE
-                UPPER(rpa.state_id_2)
-        END AS "PROVINCIA",
-        CASE
-            WHEN rpa.cautonoma_id IS NOT NULL THEN
-                (SELECT UPPER(name) FROM res_country_ca WHERE id = rpa.cautonoma_id)
-            ELSE
-                ''
-        END AS "COMUNIDAD",
-        c.name AS "PAÍS",
-        TO_CHAR(ai.date_invoice, 'MM') AS "MES",
-        TO_CHAR(ai.date_invoice, 'DD') AS "DÍA",
-        CASE
-            WHEN ai.type = 'out_invoice' THEN
-                COALESCE(ai.portes, 0) + COALESCE(ai.portes_cubiertos, 0)
-            ELSE
-                -(COALESCE(ai.portes, 0) + COALESCE(ai.portes_cubiertos, 0))
-        END AS "PORTES CARGADOS POR EL TRANSPORTISTA",
-        CASE
-            WHEN ai.type = 'out_invoice' THEN
-                COALESCE(ai.portes_cubiertos, 0)
-            ELSE
-                -(COALESCE(ai.portes_cubiertos, 0))
-        END AS "PORTES CUBIERTOS",
-        CASE
-            WHEN ai.type = 'out_invoice' THEN
-                COALESCE(ai.portes, 0)
-            ELSE
-                -(COALESCE(ai.portes, 0))
-        END AS "PORTES COBRADOS A CLIENTE",
-        TO_CHAR(ai.date_invoice, 'YYYY') AS "AÑO"
-    FROM
-        account_invoice ai
-    INNER JOIN
-        res_partner_address rpa ON rpa.id = ai.address_shipping_id
-    INNER JOIN
-        res_country c ON c.id = rpa.pais_id
-    LEFT JOIN
-        stock_sede_ps ssp ON ssp.id = ai.sede_id
-    LEFT JOIN
-        res_company rc ON rc.id = ai.company_id
-    LEFT JOIN
-        res_partner rp ON rp.id = ai.partner_id
-    WHERE
-        ai.state NOT IN ('draft', 'cancel')
-        AND ai.type IN ('out_invoice', 'out_refund')
-        AND ai.carrier_id IS NOT NULL
-        AND ai.date_invoice BETWEEN '{fecha_inicio_str}' AND '{fecha_fin_str}'
-        AND ai.obsolescencia = FALSE
-    GROUP BY
-        ai.id,
-        ai.company_id,
-        ai.date_invoice,
-        TO_CHAR(ai.date_invoice, 'YYYY'),
-        TO_CHAR(ai.date_invoice, 'MM'),
-        TO_CHAR(ai.date_invoice, 'YYYY-MM-DD'),
-        ai.carrier_id,
-        ai.partner_id,
-        ai.name,
-        ai.obsolescencia,
-        ai.type,
-        c.name,
-        rpa.state_id_2,
-        COALESCE(ai.portes, 0) + COALESCE(ai.portes_cubiertos, 0),
-        COALESCE(ai.portes_cubiertos, 0),
-        COALESCE(ai.portes, 0),
-        rc.name,
-        ssp.name,
-        rpa.prov_id,
-        rpa.cautonoma_id,
-        rp.nombre_comercial,
-        rpa.city,
-        rp.id
-    ORDER BY
-        ai.id DESC;
+    SELECT 
+    ai.id AS "ID FACTURA",
+    ai.date_invoice AS "FECHA FACTURA",
+    ai.internal_number AS "CODIGO FACTURA",
+    ai.name AS "DESCRIPCION",
+    rc.name AS "COMPAÑÍA",
+    ssp.name AS "SEDE",
+    'S-' || rp.id AS "ID BBSeeds",
+    rp.nombre_comercial AS "CLIENTE",
+    rpa.city AS "CIUDAD",
+    CASE 
+        WHEN rpa.prov_id IS NOT NULL THEN 
+            (SELECT UPPER(name) FROM res_country_provincia WHERE id = rpa.prov_id)
+        ELSE 
+            UPPER(rpa.state_id_2)
+    END AS "PROVINCIA",
+    CASE 
+        WHEN rpa.cautonoma_id IS NOT NULL THEN 
+            (SELECT UPPER(name) FROM res_country_ca WHERE id = rpa.cautonoma_id)
+        ELSE 
+            ''
+    END AS "COMUNIDAD",
+    c.name AS "PAÍS",
+    EXTRACT(MONTH FROM ai.date_invoice) AS "MES",
+	EXTRACT(DAY FROM ai.date_invoice) AS "DÍA",
+    CASE 
+        WHEN ai.type = 'out_invoice' THEN 
+            COALESCE(ai.portes, 0) + COALESCE(ai.portes_cubiertos, 0)
+        ELSE 
+            -(COALESCE(ai.portes, 0) + COALESCE(ai.portes_cubiertos, 0))
+    END AS "PORTES CARGADOS POR EL TRANSPORTISTA",
+    CASE 
+        WHEN ai.type = 'out_invoice' THEN 
+            COALESCE(ai.portes_cubiertos, 0)
+        ELSE 
+            -(COALESCE(ai.portes_cubiertos, 0))
+    END AS "PORTES CUBIERTOS",
+    CASE 
+        WHEN ai.type = 'out_invoice' THEN 
+            COALESCE(ai.portes, 0)
+        ELSE 
+            -(COALESCE(ai.portes, 0))
+    END AS "PORTES COBRADOS A CLIENTE",
+    EXTRACT(YEAR FROM ai.date_invoice) AS "AÑO"
+ 
+FROM 
+    account_invoice ai
+INNER JOIN 
+    res_partner_address rpa ON rpa.id = ai.address_shipping_id
+INNER JOIN 
+    res_country c ON c.id = rpa.pais_id
+LEFT JOIN 
+    stock_sede_ps ssp ON ssp.id = ai.sede_id
+LEFT JOIN 
+    res_company rc ON rc.id = ai.company_id
+LEFT JOIN 
+    res_partner rp ON rp.id = ai.partner_id
+ 
+WHERE 
+    ai.state NOT IN ('draft', 'cancel') 
+    AND ai.type IN ('out_invoice', 'out_refund') 
+    AND ai.carrier_id IS NOT NULL 
+    AND ai.date_invoice BETWEEN '{fecha_inicio_str}' AND '{fecha_fin_str}' 
+    AND ai.obsolescencia = FALSE -- BBSEEDS
+ 
+GROUP BY 
+    ai.id,
+    ai.company_id,
+    ai.date_invoice,
+    EXTRACT(YEAR FROM ai.date_invoice),
+    EXTRACT(MONTH FROM ai.date_invoice),
+    EXTRACT(DAY FROM ai.date_invoice),
+    ai.carrier_id,
+    ai.partner_id,
+    ai.name,
+    ai.obsolescencia,
+    ai.type,
+    c.name,
+    rpa.state_id_2,
+    rc.name,
+    ssp.name,
+    rpa.prov_id,
+    rpa.cautonoma_id,
+    rp.nombre_comercial,
+    rpa.city,
+    rp.id
+ 
+ORDER BY 
+    ai.id DESC;
     """
     
     # 4. Ejecutar la consulta
